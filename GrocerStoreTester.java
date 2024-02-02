d0d01d3ae8161d88a3ab7c5b4af0eea49525b96e
Mmahrusnaseem <102264087+Moski2204@users.noreply.github.com>
2024-02-02 02:42:30 -0500

    GroceryStoreTester.java

--- GrocerStoreTester.java
+++ GrocerStoreTester.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GrocerStoreTester {
+public class GroceryStoreTester {
 
     public static void main(String args[]) {
         GroceryStore store = new GroceryStore();
